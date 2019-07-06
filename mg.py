dd147a4001c654066110ab254fbc6d7f89143d9a
Rachida92 <rb7592@outlook.com>
2019-07-06 12:05:09 +0200

    ajout de la class perso et du laby avec numpy

--- mg.py
+++ mg.py
@@ -1,5 +1,26 @@
+import csv
 import numpy as np
 
-a = np.array([[1,2,3],[4,5,6],[4,5,6],[4,5,6],[4,5,6],[4,5,6]])
+class Personnage:
+    def __init__(self, name, image, position):
+        self.name = name
+        self.image = image
+        self.position = position
+
+
+MG = Personnage("Mac Gyver", "none", 0)
+mechant = Personnage("Mechant", "none",0)
+
+
+#print(mechant.name)
+#print(mechant.image)
+#print(mechant.position)
+
+class Labyrinthe:
+    pass
+
+    
+a = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
+
 
 print(a)
