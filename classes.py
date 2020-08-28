252590b55cc9fa8dbad20554c9b90e67e6c45f53
Rachida92 <rb7592@outlook.com>
2020-08-28 14:52:29 +0200

    constantes.py -> classes.py / création de la classe MacGyver + méthode move et initialisation du labyrinthe. Affichage MG dans laby

---
+++ classes.py
@@ -0,0 +1,54 @@
+#definition de la classe Personnage pour créer MG et le méchant
+class MacGyver:
+    def __init__(self, labyrinthe):
+        self.x = 0
+        self.y = 0
+        self.position = (self.y,self.x)
+        self.labyrinthe = labyrinthe
+        self.labyrinthe.laby[self.position[0]][self.position[1]] = "M"
+
+    def move(self,direction):
+        self.position = (self.y,self.x)
+        self.labyrinthe.laby[self.position[0]][self.position[1]] = "d"
+        if direction == "s":
+            self.position = (self.y+1,self.x)
+            self.labyrinthe.laby[self.position[0]][self.position[1]] = "M"
+        elif direction == "z":
+            self.position = (self.y-1,self.x)
+            self.labyrinthe.laby[self.position[0]][self.position[1]] = "M"
+        
+
+
+
+
+#création du labyrithe qui ouvre et lit un fichier csv qui affiche les 15 lignes et colonnes du fichier
+
+class Labyrinthe:
+    def __init__(self, textfile):
+
+        self.laby = []
+
+        with open(textfile, "r") as f:
+            for contenu in f.readlines():
+                            
+                lines = []
+                for element in contenu:
+                    if element != "\n":
+                        lines.append(element)                        
+                self.laby.append(lines)
+        
+
+    def display_laby(self):
+        for line in self.laby:
+            labyline = ""
+            for element in line:
+                labyline += element
+            print(labyline)
+
+            
+
+            
+
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-from constantes import Labyrinthe
+from classes import Labyrinthe, MacGyver
 
 level1 = Labyrinthe("carte_1.txt")
+MG = MacGyver(level1)
+
+"""
+position supposée
+x = nombre d'éléments
+y = nombre de lignes
+"""
+
+continuer = 1
+while continuer:
+    #boucle de jeu
+    level1.display_laby()
+    direction = input("Dans quelle direction voulez-vous aller : ")
+    if direction == "z": #haut
+        print("Vous voulez aller en haut")
+    elif direction == "s": #bas
+        MG.move(direction)
+    elif direction == "q": #gauche
+        print("Vous voulez aller en gauche")
+    elif direction == "d": #droite
+        print("Vous voulez aller en droite")
+
+
