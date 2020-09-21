86fb03c7694b92294de2328907a242e37b558233
Rachida92 <rb7592@outlook.com>
2020-09-21 16:01:41 +0200

    jeu fonctionnel dans le terminal/import de pygame

--- classes.py
+++ classes.py
@@ -1,20 +1,26 @@
 from random import randint
+import pygame
 
-#definition de la classe Personnage pour créer MG et le méchant
 class MacGyver:
     def __init__(self, labyrinthe):
         self.x = 0
         self.y = 0
-        
         self.labyrinthe = labyrinthe
         self.labyrinthe.laby[self.y][self.x] = "M"
-        self.objets = []
+        self.MGobjets = []
 
     def check_collision(self,a,b):
-
         if self.labyrinthe.laby[a][b] == "a":
-            self.labyrinthe.continuer = 0
-            print("vous avez gagné")
+            if len(self.MGobjets) < 3:
+                self.labyrinthe.continuer = 0
+                print("Vous avez perdu")
+            else:
+                self.labyrinthe.continuer = 0
+                print("vous avez gagné")
+                return True
+        
+        if self.labyrinthe.laby[a][b] in self.labyrinthe.objets:
+            self.MGobjets.append(self.labyrinthe.laby[a][b])
             return True
 
         if self.labyrinthe.laby[a][b] != "m":
@@ -42,25 +48,19 @@ class MacGyver:
                 self.labyrinthe.laby[self.y][self.x] = "M"
                 self.labyrinthe.laby[self.y][self.x+1] = " "
         
-        elif direction == "d": 
-                      
+        elif direction == "d":                      
             if self.check_collision(self.y,self.x+1):
                 self.x += 1
                 self.labyrinthe.laby[self.y][self.x] = "M"
                 self.labyrinthe.laby[self.y][self.x-1] = " "
 
     
-        
-        
-
-
-
-
 class Labyrinthe:
+
     def __init__(self, textfile):
         self.continuer = 1
         self.laby = []
-        self.a = 0
+        self.objets = ["T","A","E"]
         
         with open(textfile, "r") as f:
             for contenu in f.readlines():
@@ -70,32 +70,27 @@ class Labyrinthe:
                     if element != "\n":
                         lines.append(element)                        
                 self.laby.append(lines)
-        # appeler place_object ici 
+        
         
         self.place_object()
     
-
     def display_laby(self):
         for line in self.laby:
             labyline = ""
             for element in line:
                 labyline += element
             print(labyline)
-
-#début d'essai de création de la méthode
-    
+ 
     def place_object(self):
               
         i = 3
         
-
         while i > 0:
-            a = randint(0,15)
-            b = randint(0,15)
+            a = randint(0,14)
+            b = randint(0,14)
             
-            if MacGyver.check_collision(a,b):
-                #print(self.laby[a][b])
-                print(a,b)
+            if self.laby[a][b] == " ":
+                self.laby[a][b] = self.objets[i-1]
                 i -= 1
 
 
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from classes import Labyrinthe, MacGyver
+import pygame
 
 level1 = Labyrinthe("carte_1.txt")
 MG = MacGyver(level1)
 
-
-
 while level1.continuer:
-    print(level1.continuer)
     #boucle de jeu
     level1.display_laby()
     direction = input()
