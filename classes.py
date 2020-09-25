9bea465e9401210f0bee8785b7975222d5704a7e
Rachida92 <rb7592@outlook.com>
2020-09-25 22:04:19 +0200

    update Labyrinthe with pygame/first window

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 from random import randint
 import pygame
 
+
+
 class MacGyver:
     def __init__(self, labyrinthe):
         self.x = 0
@@ -8,17 +10,19 @@ class MacGyver:
         self.labyrinthe = labyrinthe
         self.labyrinthe.laby[self.y][self.x] = "M"
         self.MGobjets = []
+        self.seringue = False
 
-    def check_collision(self,a,b):
+    def check_collision(self, a, b):
         if self.labyrinthe.laby[a][b] == "a":
             if len(self.MGobjets) < 3:
-                self.labyrinthe.continuer = 0
+                self.labyrinthe.continuer = False
                 print("Vous avez perdu")
             else:
-                self.labyrinthe.continuer = 0
+                self.labyrinthe.continuer = False
+                self.seringue = True
                 print("vous avez gagné")
                 return True
-        
+
         if self.labyrinthe.laby[a][b] in self.labyrinthe.objets:
             self.MGobjets.append(self.labyrinthe.laby[a][b])
             return True
@@ -26,79 +30,86 @@ class MacGyver:
         if self.labyrinthe.laby[a][b] != "m":
             return True
 
-    def move(self,direction):  
+    def move(self, direction):
 
         if direction == "s":
-            if self.check_collision(self.y+1,self.x):
+            if self.check_collision(self.y + 1, self.x):
                 self.y += 1
                 self.labyrinthe.laby[self.y][self.x] = "M"
-                self.labyrinthe.laby[self.y-1][self.x] = " "
+                self.labyrinthe.laby[self.y - 1][self.x] = " "
 
-            #voir pour remplacer previous y par un   ici.
- 
-        elif direction == "z":            
-            if self.check_collision(self.y-1,self.x):
+            # voir pour remplacer previous y par un   ici.
+
+        elif direction == "z":
+            if self.check_collision(self.y - 1, self.x):
                 self.y -= 1
                 self.labyrinthe.laby[self.y][self.x] = "M"
-                self.labyrinthe.laby[self.y+1][self.x] = " "
-        
-        elif direction == "q":            
-            if self.check_collision(self.y,self.x-1):
+                self.labyrinthe.laby[self.y + 1][self.x] = " "
+
+        elif direction == "q":
+            if self.check_collision(self.y, self.x - 1):
                 self.x -= 1
                 self.labyrinthe.laby[self.y][self.x] = "M"
-                self.labyrinthe.laby[self.y][self.x+1] = " "
-        
-        elif direction == "d":                      
-            if self.check_collision(self.y,self.x+1):
+                self.labyrinthe.laby[self.y][self.x + 1] = " "
+
+        elif direction == "d":
+            if self.check_collision(self.y, self.x + 1):
                 self.x += 1
                 self.labyrinthe.laby[self.y][self.x] = "M"
-                self.labyrinthe.laby[self.y][self.x-1] = " "
+                self.labyrinthe.laby[self.y][self.x - 1] = " "
 
-    
+
 class Labyrinthe:
 
     def __init__(self, textfile):
-        self.continuer = 1
+        self.continuer = True
         self.laby = []
-        self.objets = ["T","A","E"]
+        self.objets = ["T", "A", "E"]
+        self.taille_pixels = 30
         
+
         with open(textfile, "r") as f:
             for contenu in f.readlines():
-                            
+
                 lines = []
                 for element in contenu:
                     if element != "\n":
-                        lines.append(element)                        
+                        lines.append(element)
                 self.laby.append(lines)
-        
-        
+
         self.place_object()
-    
-    def display_laby(self):
-        for line in self.laby:
-            labyline = ""
-            for element in line:
-                labyline += element
-            print(labyline)
- 
-    def place_object(self):
-              
-        i = 3
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((640, 480))
         
+
+    def display_laby(self):
+        for k,line in enumerate(self.laby):
+            for h,element in enumerate(line):
+                if element == 'm':
+                    self.screen.blit('mur.png',(h*self.taille_pixels,k*self.taille_pixels))
+                #elif element == 'a':
+
+
+
+    def place_object(self):
+
+        i = 3
+
         while i > 0:
-            a = randint(0,14)
-            b = randint(0,14)
-            
+            a = randint(0, 14)
+            b = randint(0, 14)
+
             if self.laby[a][b] == " ":
-                self.laby[a][b] = self.objets[i-1]
+                self.laby[a][b] = self.objets[i - 1]
                 i -= 1
 
 
-        
-           
-
-            
-
-
-
-
+"""
+        #premier essai ajout d'une image de mur
+        mur = pygame.image.load("mur.png").convert()
+        for i in self.laby:
+            if i == 'm':
+                fenetre.blit(mur, (0,0))
+"""
--- main.py
+++ main.py
@@ -4,19 +4,32 @@ import pygame
 level1 = Labyrinthe("carte_1.txt")
 MG = MacGyver(level1)
 
+"""
+Création de l'interface graphique avec pygame
+
+
+
+pygame.init()
+fenetre = pygame.display.set_mode((640, 480))
+
+pygame.display.flip()
+
 while level1.continuer:
-    #boucle de jeu
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            level1.continuer = False
+            pygame.quit()
+"""
+
+while level1.continuer:
+
     level1.display_laby()
     direction = input()
-    if direction == "z": #haut
+    if direction == "z":  # haut
         MG.move(direction)
-    elif direction == "s": #bas
+    elif direction == "s":  # bas
         MG.move(direction)
-    elif direction == "q": #gauche
+    elif direction == "q":  # gauche
         MG.move(direction)
-    elif direction == "d": #droite
+    elif direction == "d":  # droite
         MG.move(direction)
-
-
-
-
