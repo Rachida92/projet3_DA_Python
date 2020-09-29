11060eb9aa32f8c3e41b98f382b121de63c3f7d6
Rachida92 <rb7592@outlook.com>
2020-09-29 22:48:33 +0200

    one file by class created/first pygame window from labyrinthe class

--- macgyver.py
+++ macgyver.py
@@ -1,8 +1,3 @@
-from random import randint
-import pygame
-
-
-
 class MacGyver:
     def __init__(self, labyrinthe):
         self.x = 0
@@ -57,59 +52,3 @@ class MacGyver:
                 self.x += 1
                 self.labyrinthe.laby[self.y][self.x] = "M"
                 self.labyrinthe.laby[self.y][self.x - 1] = " "
-
-
-class Labyrinthe:
-
-    def __init__(self, textfile):
-        self.continuer = True
-        self.laby = []
-        self.objets = ["T", "A", "E"]
-        self.taille_pixels = 30
-        
-
-        with open(textfile, "r") as f:
-            for contenu in f.readlines():
-
-                lines = []
-                for element in contenu:
-                    if element != "\n":
-                        lines.append(element)
-                self.laby.append(lines)
-
-        self.place_object()
-
-        pygame.init()
-
-        self.screen = pygame.display.set_mode((640, 480))
-        
-
-    def display_laby(self):
-        for k,line in enumerate(self.laby):
-            for h,element in enumerate(line):
-                if element == 'm':
-                    self.screen.blit('mur.png',(h*self.taille_pixels,k*self.taille_pixels))
-                #elif element == 'a':
-
-
-
-    def place_object(self):
-
-        i = 3
-
-        while i > 0:
-            a = randint(0, 14)
-            b = randint(0, 14)
-
-            if self.laby[a][b] == " ":
-                self.laby[a][b] = self.objets[i - 1]
-                i -= 1
-
-
-"""
-        #premier essai ajout d'une image de mur
-        mur = pygame.image.load("mur.png").convert()
-        for i in self.laby:
-            if i == 'm':
-                fenetre.blit(mur, (0,0))
-"""
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from classes import Labyrinthe, MacGyver
+from labyrinthe import Labyrinthe
+from macgyver import MacGyver
 import pygame
 
 level1 = Labyrinthe("carte_1.txt")
@@ -13,17 +14,17 @@ pygame.init()
 fenetre = pygame.display.set_mode((640, 480))
 
 pygame.display.flip()
-
+"""
 while level1.continuer:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             level1.continuer = False
             pygame.quit()
-"""
+
 
 while level1.continuer:
 
-    level1.display_laby()
+    #level1.display_laby()
     direction = input()
     if direction == "z":  # haut
         MG.move(direction)
